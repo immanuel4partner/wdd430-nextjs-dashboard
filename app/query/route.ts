d618b6e06de798a415879ba9e04cf85899dd06b0
immanuel4partner <immanuel4partner@gmail.com>
2026-07-18 14:38:30 +0100

    Add NextAuth authentication and protected dashboard routes

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,30 @@
-// import postgres from 'postgres';
+import { sql } from '@vercel/postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+async function listInvoices() {
+  try {
+    const data = await sql`
+      SELECT invoices.amount, customers.name
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      WHERE invoices.amount = 666;
+    `;
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice data.');
+  }
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json(
+      { error },
+      {
+        status: 500,
+      },
+    );
+  }
 }
